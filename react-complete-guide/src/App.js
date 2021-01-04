1f879fe3abe90cf126c75285fef5e227443c9513
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-04 10:38:51 +0800

    props and state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = () => {
+    //console.log('was clicked');
+    //Do Not Do This : this.state.persons[0].name = 'Maximillian';
+    this.setState({persons: [
+      {name: 'Maximillian', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ] });
+  }
+
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am React File</h1>
-    //   </div>
-    // );
-    return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'I am an react Element'));
+     return (
+       <div className="App">
+         <h1>Hi, I am React File</h1>
+         <p>Hi There also</p>
+         <button onClick={this.switchNameHandler}>Switch Name</button>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       </div>
+     );
+    //return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'I am an react Element'));
   }
 }
 
