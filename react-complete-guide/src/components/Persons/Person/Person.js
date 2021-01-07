d27571bdbba5c8a290dd89062d964eb3b4989db5
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-07 18:48:03 +0800

    intor to fragment

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 //import Radium from 'radium';
 //import styled from 'styled-components';
 import Aux from '../../../hoc/Auxiliary';
@@ -9,7 +9,7 @@ class Person extends Component {
     render(){ 
     console.log('[Person.js] rendering..'); 
     return (
-        <Aux>
+        <Fragment>
             <p key="i1" onClick={this.props.click}>
                 I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p key="i2">{this.props.children}</p>
@@ -18,7 +18,7 @@ class Person extends Component {
                 type="text" 
                 onChange={this.props.changed} 
                 value={this.props.name}/>
-        </Aux>
+        </Fragment>
         
     );
     }
