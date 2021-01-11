89050cd78534001b44fc8b020704a4a1b3c11fe3
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-11 15:51:07 +0800

    Side Drawer implementation

--- public/src/components/Navigation/SideDrawer/SideDrawer.js
+++ public/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop'; 
+import Aux from '../../../hoc/Auxiliary';
 import NavigationItems from '../NavigationItems/NavigationItems';
+
 
 import classes from './SideDrawer.css';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+        
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop 
+                show={props.open} 
+                clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
