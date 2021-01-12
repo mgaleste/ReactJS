4f1de85bdbbbd248703db02ef7338c8a49154297
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-12 09:50:37 +0800

    folder restructure

--- public/src/components/Navigation/SideDrawer/SideDrawer.js
+++ public/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop'; 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 
