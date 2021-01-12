289be5b8978c4f1f528f4dde3598ab2b9f0845f0
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-12 14:44:24 +0800

    changes

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
+    state ={
+        posts: []
+    }
+    componentDidMount(){
+        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response =>{
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {...post,
+                    author: 'Mixerwars'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+            });
+        
+    }
+    
     render () {
+        const posts = this.state.posts.map(post =>{
+            return <Post 
+                key={post.id} 
+                title={post.title}
+                author={post.author}/>
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
