7aa2923d57899fc4f7bf42b66ab6b33d589510d6
Michael Angelo Galeste <Michael.Angelo.Galeste@wdc.com>
2021-01-18 18:13:23 +0800

    routes

--- router/src/containers/Blog/Blog.js
+++ router/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 // import axios from 'axios';
 import axios from '../../axios';
 
@@ -16,8 +16,12 @@ class Blog extends Component {
                 <header>
                         <nav>
                             <ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/new-post">New Post</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#Submit',
+                                    search: '?quick=true'
+                                }}>New Post</Link></li>
                             </ul>
                         </nav>
                 </header>
@@ -26,6 +30,8 @@ class Blog extends Component {
                 */ }
                 <Route path="/" exact component={Posts}/>
                 <Route path="/new-post" exact component={NewPost}/>
+                
+                
                 </div>
         );
     }
