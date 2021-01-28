022d7a006b42f17972dd8b774af15fc35b1eb05e
mgaleste <mgaleste@gmail.com>
2021-01-29 07:59:56 +0800

    redux utility

--- redux/src/store/reducers/result.js
+++ redux/src/store/reducers/result.js
@@ -1,28 +1,36 @@
-import * as actionTypes from '../actions/action'
+import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility';
 
 const initialState = {
     
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+} ;
+
 const reducer = (state = initialState,action) => {
     switch(action.type){
         
         case(actionTypes.STORE_RESULT):
-        
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value:action.result})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value:action.result})})
+            // return {
+            //     ...state,
+            //     results: state.results.concat({id: new Date(), value:action.result})
+            // }
         case(actionTypes.DELETE_RESULT):
+            return deleteResult(state, action);
             //const id
             //const newArray = [...state.results];
             //newArray.splice(id,1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }   
+            //const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            //return updateObject(state, {results: updatedArray})
+            // return {
+            //     ...state,
+            //     results: updatedArray
+            // }   
         default:
             return state;        
     }
