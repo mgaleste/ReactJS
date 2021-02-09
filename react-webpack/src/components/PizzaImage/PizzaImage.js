a4660838f0743aef10cf675a5bd80928bb319b37
mgaleste <mgaleste@gmail.com>
2021-02-09 12:59:28 +0800

    292021

---
+++ react-webpack/src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import classes from './PizzaImage.css'
+import PizzaImage from '../../assets/pizza.jpg'
+
+const pizzaImage = (props) => (
+    <div >
+        <img src={PizzaImage} />
+    </div>
+);
+
+export default pizzaImage;
