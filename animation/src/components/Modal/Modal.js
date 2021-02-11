95cbb11a8fa2a957cf30eb3fb4d3b1af3f9d8d6d
mgaleste <mgaleste@gmail.com>
2021-02-11 14:06:12 +0800

    animation

--- animation/src/components/Modal/Modal.js
+++ animation/src/components/Modal/Modal.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+    const cssClasses = ['Modal', 
+    props.show==='entering' ? 'ModalOpen' : props.show==='exiting' ? 'ModalClose' : null];
     
     return (
     <div className={cssClasses.join(' ')}>
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>Dismiss</button>
     </div>
+    
     );
 };
 
