12ab8a872c10402a22a28636c7c75f3cab178434
mgaleste <mgaleste@gmail.com>
2021-02-24 16:33:54 +0800

    converted to hooks

--- hookburger/src/hoc/Layout/Layout.js
+++ hookburger/src/hoc/Layout/Layout.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
-import Aux from '../Aux/Aux';
+import Aux from '../Auxilliary/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
+const layout = props =>  {
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
+    
+    const sideDrawerClosedHandler = () => {
+        setShowSideDrawer(false);
     }
 
-    sideDrawerClosedHandler = () => {
-        this.setState( { showSideDrawer: false } );
+    const sideDrawerToggleHandler = () => {
+        setShowSideDrawer(!showSideDrawer);
     }
 
-    sideDrawerToggleHandler = () => {
-        this.setState( ( prevState ) => {
-            return { showSideDrawer: !prevState.showSideDrawer };
-        } );
-    }
-
-    render () {
+    
         return (
             <Aux>
                 <Toolbar
-                    isAuth={this.props.isAuthenticated}
-                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                    isAuth={props.isAuthenticated}
+                    drawerToggleClicked={sideDrawerToggleHandler} />
                 <SideDrawer
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler} />
+                    isAuth={props.isAuthenticated}
+                    open={showSideDrawer}
+                    closed={sideDrawerClosedHandler} />
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         )
-    }
+    
 }
 
 const mapStateToProps = state => {
@@ -45,4 +41,4 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect( mapStateToProps )( Layout );
+export default connect( mapStateToProps )( layout );
--- hookburger/src/store/actions/auth.js
+++ hookburger/src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDu56rOe0KWP0SWnTZgm3bJwecKWo0I9fs';
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDu56rOe0KWP0SWnTZgm3bJwecKWo0I9fs';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC6wGgmiClW29LK-NCyQdYH0q0wVFP5bdI';
+        if(!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC6wGgmiClW29LK-NCyQdYH0q0wVFP5bdI';
         }
         axios.post(url, authData)
             .then(response => {
